e35441939d8464070d856febb7f3079a69de60ff
Mariana Cardoso <marianacardoso.tds2.senai@gmail.com>
2024-10-10 14:59:46 -0300

    feat: create get method

--- src/routes/suspeitos.routes.js
+++ src/routes/suspeitos.routes.js
@@ -2,5 +2,20 @@ import { Router } from "express";
 
 const suspeitosRoutes = Router();
 
+let suspeitos = [
+    {
+        id: Math.floor(Math.random() * 1000000),
+        nome: "Sophia Gomes",
+        profissão: "Esteticista",
+        apostas: "sim",
+        nível: "alto",
+    }
+]
+
+// Rota para listar todos os candidatos
+suspeitosRoutes.get("/", (req, res) => {
+    return res.status(200).json(suspeitos);
+});
+
 
 export default suspeitosRoutes;
